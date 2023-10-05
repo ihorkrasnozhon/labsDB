470cbbb59aeb66294c493e88f55eb3d6b9f87ca9
ihorkrasnozhon <worldofiigor@gmail.com>
2023-10-05 15:44:48 +0200

    lab5

---
+++ LAB5/lab5.4.sql.4
@@ -0,0 +1,4 @@
+SELECT region, SUM(population) AS total_population
+FROM cities
+GROUP BY region
+HAVING SUM(population) > 300000
--- LAB5/lab5.5.sql
+++ LAB5/lab5.5.sql
@@ -1,8 +1,8 @@
 SELECT name, population
 FROM cities
-WHERE region IN (
-    SELECT region
-    FROM cities
-    GROUP BY region
-    HAVING COUNT(DISTINCT name) <= 5 AND population NOT BETWEEN 150000 AND 500000
+WHERE population NOT BETWEEN 150000 AND 500000
+AND region IN (
+    SELECT uuid
+    FROM regions
+    WHERE area_quantity <= 5
 )
