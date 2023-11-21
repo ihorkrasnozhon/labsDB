5ef2608d4bd794371f680c1549c8fff9f598c8dc
ihorkrasnozhon <worldofiigor@gmail.com>
2023-11-21 08:48:00 +0100

    Update lab8.sql
    table_names -> table_name
    table_id -> id
    ADDED: task-user conn, project-user conn, file-task-user conn

--- LAB8/lab8.sql
+++ LAB8/lab8.sql
@@ -1,43 +1,61 @@
 
-CREATE TABLE Users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS user_projects;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS task_users;
+DROP TABLE IF EXISTS files;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL
- 
-)
+);
 
+CREATE TABLE projects (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE Projects (
-    project_id INT PRIMARY KEY AUTO_INCREMENT,
-    project_name VARCHAR(255) NOT NULL
-   
-)
+CREATE TABLE user_projects (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    project_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+);
 
-
-CREATE TABLE Tasks (
-    task_id INT PRIMARY KEY AUTO_INCREMENT,
-    task_name VARCHAR(255) NOT NULL,
+CREATE TABLE tasks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
     author_id INT,
     project_id INT,
-    FOREIGN KEY (author_id) REFERENCES Users(user_id),
-    FOREIGN KEY (project_id) REFERENCES Projects(project_id)
+    FOREIGN KEY (author_id) REFERENCES users(id),
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+);
 
-)
-
-
-CREATE TABLE Task_Users (
-    task_user_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE task_users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     task_id INT,
     user_id INT,
-    FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
-)
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
-CREATE TABLE Files (
-    file_id INT PRIMARY KEY AUTO_INCREMENT,
-    file_name VARCHAR(255) NOT NULL,
+CREATE TABLE files (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
     file_url VARCHAR(255) NOT NULL,
     project_id INT,
     task_id INT,
-    FOREIGN KEY (project_id) REFERENCES Projects(project_id),
-    FOREIGN KEY (task_id) REFERENCES Tasks(task_id)
+    task_users_id INT,
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (task_users_id) REFERENCES task_users(id)
 );
+
