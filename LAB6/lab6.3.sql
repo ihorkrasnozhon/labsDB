253e26aa0f38ef0e0c8b0ad6cf0249c66eb39f1a
ihorkrasnozhon <worldofiigor@gmail.com>
2023-11-28 00:54:31 +0100

    Edited

--- LAB6/lab6.3.sql
+++ LAB6/lab6.3.sql
@@ -16,43 +16,16 @@ CREATE TABLE stations (
     id VARCHAR(10) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     line_id INT NOT NULL,
+    previous_station varchar(255),
+    next_station varchar(255),
     FOREIGN KEY (line_id) REFERENCES metro_lines(id),
     transfer_to_station_id INT DEFAULT NULL
 );
 
-INSERT INTO metro_lines(name, color) VALUES
-('Холодногірсько-Заводська' , 'Червоний'),
-('Салтівська' , 'Синій'),
-('Олексіївська' , 'Зелений');
-
-INSERT INTO stations(id, name, line_id, transfer_to_station_id)
-VALUES
-('1.01', 'Холодна гора' , 1, NULL),
-('1.02', 'Південний вокзал' , 1, NULL),
-('1.03', 'Центральний ринок' , 1, NULL),
-('1.04', 'Майдан Конституції' , 1, 12),
-('1.05', 'Проспект Гагаріна' , 1, NULL),
-('1.06', 'Спортивна' , 1, 20),
-('1.07', 'Турбоатом' , 1, NULL),
-('1.08', 'Дворець спорта' , 1, NULL),
-('1.09', 'Армійська' , 1, NULL),
-('1.10', 'Тракторний завод' , 1, NULL),
-('1.11', 'Індустріальна' , 1, NULL),
-('2.01', 'Історичний музей' , 2, 4),
-('2.02', 'Університет' , 2, 23),
-('2.03', 'Пушкінська' , 2, NULL),
-('2.04', 'Київська' , 2, NULL),
-('2.05', 'Академіка Барабашова' , 2, NULL),
-('2.06', 'Академіка Павлова' , 2, NULL),
-('2.07', 'Студентська' , 2, NULL),
-('2.08', 'Героїв Праці' , 2, NULL),
-('3.01', 'Метробудівників' , 3, 6),
-('3.02', 'Захисників України' , 3, NULL),
-('3.03', 'Архітектора Бекетова' , 3, NULL),
-('3.04', 'Держпром' , 3, 13),
-('3.05', 'Наукова' , 3, NULL),
-('3.06', 'Ботанічний сад' , 3, NULL),
-('3.07', '23 Серпня' , 3, NULL),
-('3.08', 'Олексіївська' , 3, NULL),
-('3.09', 'Перемога' , 3, NULL);
+CREATE TABLE connections (
+  station_id_1 int(10) UNSIGNED NOT NULL,
+  station_id_2 int(10) UNSIGNED NOT NULL,
+  FOREIGN KEY (station_id_1) REFERENCES stations(id),
+  FOREIGN KEY (station_id_2) REFERENCES stations(id)
+);
 
--- LAB8/lab8.sql
+++ LAB8/lab8.sql
@@ -1,16 +1,15 @@
-
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS projects;
-DROP TABLE IF EXISTS user_projects;
+DROP TABLE IF EXISTS user_tasks;
 DROP TABLE IF EXISTS tasks;
-DROP TABLE IF EXISTS task_users;
-DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS project_users;
+DROP TABLE IF EXISTS project_files;
+DROP TABLE IF EXISTS task_files;
 
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -22,12 +21,12 @@ CREATE TABLE projects (
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE user_projects (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE user_tasks (
     user_id INT,
-    project_id INT,
+    task_id INT,
+    PRIMARY KEY (user_id, task_id),
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (project_id) REFERENCES projects(id)
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
 
 CREATE TABLE tasks (
@@ -39,23 +38,27 @@ CREATE TABLE tasks (
     FOREIGN KEY (project_id) REFERENCES projects(id)
 );
 
-CREATE TABLE task_users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    task_id INT,
+CREATE TABLE project_users (
+    project_id INT,
     user_id INT,
-    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    PRIMARY KEY (project_id, user_id),
+    FOREIGN KEY (project_id) REFERENCES projects(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE files (
+CREATE TABLE project_files (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     file_url VARCHAR(255) NOT NULL,
     project_id INT,
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+);
+
+CREATE TABLE task_files (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
     task_id INT,
-    task_users_id INT,
-    FOREIGN KEY (project_id) REFERENCES projects(id),
-    FOREIGN KEY (task_id) REFERENCES tasks(id),
-    FOREIGN KEY (task_users_id) REFERENCES task_users(id)
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
 
